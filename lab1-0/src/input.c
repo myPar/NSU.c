b96f3be7c80312e32f66ee786b1a83f894a62ab7
a.bobrovskii1@g.nsu.ru <a.bobrovskii1@g.nsu.ru>
2020-06-11 13:06:27 +0700

    commit message

---
+++ lab1-0/src/input.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+FILE *set_input(int argc, char *argv[]) {
+    FILE *input = NULL;
+
+    if (argc > 1) {
+        if (argc != 2) {
+            printf("incorrect number of arguments: \nshould be input file name or no arguments");
+            return NULL;
+        }
+        char *input_file_name = argv[1];
+
+        if (!(input = fopen(input_file_name, "rt"))) {
+            printf("can't open the file for reading");
+            return NULL;
+        }
+    }
+    else {
+        input = stdin;
+    }
+
+    return input;
+}
--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -1,41 +1,29 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include "input.h"
 
 int main(int argc, char *argv[]) {
-    char template[20] = {0};
-    char source[128] = {0};
-    int offset_arr[256] = {0};
+#define temp_size 20
+#define src_size 128
+#define range 256
+    char template[temp_size] = {0};
+    char source[src_size] = {0};
+    int offset_arr[range] = {0};
     int temp_length = 0;
+    FILE *input;
 
-    FILE *input = NULL;
-    char *input_file_name = NULL;
-
-    if (argc > 1) {
-        if (argc != 2) {
-            printf("incorrect number of arguments: \nshould be input file name or no arguments");
-            exit(1);
-        }
-        input_file_name = argv[1];
-
-        if (!(input = fopen(input_file_name, "rt"))) {
-            printf("can't open the file for reading");
-            exit(1);
-        }
+    if ((input = set_input(argc, argv)) == NULL) {
+        return 1;
     }
-    else {
-        input = stdin;
-    }
-    if (!fgets(template, 20, input)) {
+    if (!fgets(template, temp_size, input)) {
         printf("can't read a string\n");
-        exit(1);
+        return 1;
     }
-
     // cut '\n' from the template
     template[strlen(template) - 1] = '\0';
     temp_length = strlen(template);
     // initialize start characters offset: the template length
-    for (int i = 0; i < 256; i++) {
+    for (int i = 0; i < range; i++) {
         offset_arr[i] = temp_length;
     }
     // initialize offset of template alphabet
@@ -51,7 +39,7 @@ int main(int argc, char *argv[]) {
 
     while (!feof(input)) {
         // get new source
-        if (!fgets(source, 128, input)) {
+        if (!fgets(source, src_size, input)) {
             break;
         }
         // current source length
@@ -78,6 +66,10 @@ int main(int argc, char *argv[]) {
             cur_text_pos += offset;
         }
     }
+#undef temp_size
+#undef src_size
+#undef range
+
     if (input != stdin) {
         fclose(input);
     }
