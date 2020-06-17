bd61ced14fa67cd835eb4c9b508e7c9d46e80216
Andrey Bobrovskii <a.bobrovskii1@g.nsu.ru>
2020-06-17 22:08:13 +0700

    Revert "commit message"
    This reverts commit da2fe923feb932a6c7385f250d4b5a068550e637

--- lab4/src/input.h
+++ lab4/src/input.h
@@ -1,4 +1,7 @@
 #pragma once
+#ifndef POSTFIX_NOTATION_INPUT_H
+#define POSTFIX_NOTATION_INPUT_H
 #include <stdio.h>
 FILE *set_input(int argc, char *argv[]);
 void unset_input(FILE *input);
+#endif
